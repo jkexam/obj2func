d2073e5fda47e02fa557bcc8682c1e790cf7427e
JKOOGI-HP\jkoog <jkoogi@naver.com>
2025-03-25 22:58:31 +0900

    * init

---
+++ src/main/kotlin/com/jk/obj2func/Obj2funcApplication.kt
@@ -0,0 +1,11 @@
+package com.jk.obj2func
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Obj2funcApplication
+
+fun main(args: Array<String>) {
+	runApplication<Obj2funcApplication>(*args)
+}
